afb6683b508ab61155553ef1ed2f6ec132ab8a60
Yiyi <yiyiwzhang@gmail.com>
2015-08-27 11:44:23 -0400

    BROKE SOMETHING DOING JS ANIMATION WTF!!

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,14 @@
-//$(function(){
+$(function(){
 
-/*ANIMATED BOXES 
-	$(".animatedcontainer1").hover(
+//ANIMATED BOXES 
+	$(".animatedcontainer").hover(
 		function(){//mousein
-			$(".animatedpanel1").animate(
+			$(".animatedpanel").animate(
 				{height: "40%"},600)
 			},
 		function(){//mouseout
-			$(".animatedpanel1").animate(
+			$(".animatedpanel").animate(
 				{height: "5%"},600)
 		});
 
-	$(".animatedcontainer2").hover(
-		function(){//mousein
-			$(".animatedpanel2").animate(
-				{height: "40%"},600)
-			},
-		function(){//mouseout
-			$(".animatedpanel2").animate(
-				{height: "5%"},600)
-		});
-
-	$(".animatedcontainer3").hover(
-		function(){//mousein
-			$(".animatedpanel3").animate(
-				{height: "40%"},600)
-			},
-		function(){//mouseout
-			$(".animatedpanel3").animate(
-				{height: "5%"},600)
-		});
-
-});*/
+});
